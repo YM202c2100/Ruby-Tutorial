df150bfb463440159d89d407a3b19fa19eb05c53
YM202c2100 <Y.M202c2100@gmail.com>
2022-11-24 09:33:44 +0900

    リスト2.6 (ここからrails第5版の内容)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :users
   # Defines the root path route ("/")
-  root "application#hello"
+  root "users#index"
 end
